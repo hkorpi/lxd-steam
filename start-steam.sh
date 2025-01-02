3cfe41c0c0cd5753c1de05e4cd3254798cf3cf27
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2025-01-02 21:05:11 +0200

    fix render permissions before starting steam

--- start-steam.sh
+++ start-steam.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 cd $(dirname $0)
 
+sudo ./fix-render-permission.sh
 systemctl --user import-environment DISPLAY
 PULSE_SERVER=unix:/home/ubuntu/pulse-native __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia steam
