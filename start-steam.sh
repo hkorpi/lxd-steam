68f2197b737a7d567bf720aa1e799661f5c1eb65
Mika Haapakorpi <mika.haapakorpi@solita.fi>
2026-01-06 01:09:10 +0200

    sudo steamdeps - otherwise deps installation fails

--- start-steam.sh
+++ start-steam.sh
@@ -5,4 +5,5 @@ cd $(dirname $0)
 
 sudo ./fix-render-permission.sh
 systemctl --user import-environment DISPLAY
+sudo steamdeps --no-install-confirmation
 PULSE_SERVER=unix:/home/ubuntu/pulse-native __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia steam
